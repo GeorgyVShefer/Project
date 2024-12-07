042f296a6f190677e466490a9a61c9b159e3d3ec
GeorgyVShefer <156106194+GeorgyVShefer@users.noreply.github.com>
2024-12-07 20:28:30 +0300

    TASK_0004 Реализовать метод save для таблицы authors  - Создать пакет dao(Описать логику сохранения данных в таблицу Author)  - Создать пакет service(Описать метод, который сохраняет полученные от клиента данные в объект AuthorSaveRq сохраняет их в таблицу authors и возвращает ответ пользователю из объекта AuthorSaveRs, с аналогичными полями! Т.е. на вход ты принимаешь AuthorSaveRq, а на выход отдаешь AuthorSaveRs с такими же полями, что и на входе)  - Создать пакеты dto(в нем создать объекты AuthorSaveRq (request) и AuthorSaveRs(respons) поля у них будут одинаковыми)  - Создать пакет controller(реализовать объект servlet'a AuthorController в котором будет метод, который будет возвращать AuthorSaveRs)  - Покрыть логику тестами(сервис покрыть тестами обязательно!!)  - Дополнительно проверить через postman

---
+++ src/main/java/org/example/dto/AuthorGetByIdRs.java
@@ -0,0 +1,31 @@
+package org.example.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.model.Book;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthorGetByIdRs {
+    private int id;
+    private String name;
+    private List<Book> books;
+    @Override
+    public String toString() {
+        StringBuilder bookTitles = new StringBuilder();
+        for (Book book : books) {
+            if (bookTitles.length() > 0) bookTitles.append(", ");
+            bookTitles.append(book.getTitle());
+        }
+        return "Author{id=" + id + ", name='" + name + "', books=[" + bookTitles + "]}";
+    }
+}
---
+++ src/main/java/org/example/dto/AuthorSaveRs.java
@@ -0,0 +1,27 @@
+package org.example.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import org.example.model.Book;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class AuthorSaveRs {private int id;
+    private String name;
+    private List<Book> books;
+    @Override
+    public String toString() {
+        StringBuilder bookTitles = new StringBuilder();
+        for (Book book : books) {
+            if (bookTitles.length() > 0) bookTitles.append(", ");
+            bookTitles.append(book.getTitle());
+        }
+        return "Author{id=" + id + ", name='" + name + "', books=[" + bookTitles + "]}";
+    }
+}
